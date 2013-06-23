08d00acf8cba66e95a868a97f6bf50413368441e
Luke Huckins <luke.huckins@gmail.com>
2013-06-23 14:30:47 -0700

    Added welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsinstallerOsxDemo::Application.routes.draw do
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -47,9 +49,8 @@ RailsinstallerOsxDemo::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  # just remember to delete public/index.html
+root :to => 'welcome#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
